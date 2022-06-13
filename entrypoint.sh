019f0585a62607880d20ab43dcf14c265e367c39
Ian Lewis <ianmlewis@gmail.com>
2022-06-13 23:03:22 +0000

    Use reviewdog exit code for the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,18 @@ git config --global --add safe.directory $GITHUB_WORKSPACE
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+echo '::group:: Running yamllint with reviewdog 🐶 ...'
 yamllint --version
+yamllint --format "parsable" ${INPUT_YAMLLINT_FLAGS:-'.'} |
+    reviewdog \
+        -efm="%f:%l:%c: %m" \
+        -name "yamllint" \
+        -reporter="${INPUT_REPORTER:-github-pr-check}" \
+        -level="${INPUT_LEVEL}" \
+        -filter-mode="${INPUT_FILTER_MODE}" \
+        -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+        ${INPUT_REVIEWDOG_FLAGS}
+EXIT_CODE=$?
+echo '::endgroup::'
 
-yamllint --format "parsable" ${INPUT_YAMLLINT_FLAGS:-'.'} \
-  | reviewdog \
-    -efm="%f:%l:%c: %m" \
-    -name "yamllint" \
-    -reporter="${INPUT_REPORTER:-github-pr-check}" \
-    -level="${INPUT_LEVEL}" \
-    -filter-mode="${INPUT_FILTER_MODE}" \
-    -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
-    ${INPUT_REVIEWDOG_FLAGS}
+exit $EXIT_CODE
